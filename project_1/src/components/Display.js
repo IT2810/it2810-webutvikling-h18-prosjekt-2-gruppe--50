8b78438c9d014fb70d249ccca98995cfcfd07f38
Dora Oline <doraoline@gmail.com>
2018-09-05 09:56:51 +0200

    Can get svg-file

--- project_1/src/components/Display.js
+++ project_1/src/components/Display.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import SVG from './SVG.js'
 
 class Display extends Component {
+  constructor() {
+    super();
+    this.data = {
+      image: null,
+      sound: null,
+
+    }
+  }
+
+  componentDidMount() {
+    /*axios.get('https://dyrebar.no/media/encyclopedia/2017/11/01/e2b47f324e6e40a68322f06b2887beff.jpg.1110x625_q85.jpg')
+      .then(res => {
+        this.setState({image: res.data})
+      })*/
+  }
+
   render() {
+    //var img = require('../images/$(this.state.image}')
     return (
-      <h1> The main display </h1>
+      <div>
+        <h1> The main display </h1>
+
+      <SVG name="test"/>
+      </div>
     )
   }
 }
---
+++ project_1/src/components/SVG.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class SVG extends Component {
+  constructor() {
+    super();
+    this.state = {
+      image: null
+    }
+
+    this.getSVG = this.getSVG.bind(this);
+  }
+
+  componentDidMount() {
+    this.getSVG(this.props.name);
+  }
+
+  getSVG(name) {
+    axios.get("images/" + name + ".svg")
+      .then(res => {
+        console.log(res)
+        this.setState({image: res.data})
+
+        var div = document.getElementById("svg-div")
+        div.innerHTML = res.data
+      })
+  }
+  render() {
+    return (
+      <div id="svg-div">
+      </div>
+    )
+  }
+}
+
+export default SVG;
+
